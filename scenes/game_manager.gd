5983e3e9471d34ea1225ad71ba11f030210aae37
Arjun Kandadai <arjun.kandadai@gmail.com>
2025-03-13 12:35:20 -0400

    Basic camera follow added. Camera flips following direction character is facing. Must confirm hard x camera limits.

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -3,6 +3,41 @@ extends Node
 @onready var damage_timer = $DamageCooldownTimer  # Reference to the timer
 @onready var main_char = %"main_character"
 
+@onready var player = get_node("../main_character")
+@onready var camera = get_node("../MovingCamera")
+@onready var y_pos = player.position.y;
+
+func _process(delta):
+	#print("Player x Vel: ",player.velocity.x)
+	print("Player x Pos: ", player.position.x)
+	print("Camera x Pos: ", camera.position.x)
+
+	if player.position.y < 2 and player.position.y > -1:
+		camera.position.y = 420
+
+	if player.is_on_floor():
+		y_pos = player.position.y;
+		#camera.position.y = -400;
+		
+	if not player.is_on_floor():
+		if player.position.y > y_pos or player.position.y < y_pos:
+			camera.position.y += player.velocity.y/100;
+			#if camera.position.y >= 405: 
+				#camera.position.y = 405
+	
+	if Input.is_action_pressed("ui_right"):
+		if player.position.x < 100000 && player.position.x > 40:
+			if player.velocity[0] != 0:
+				camera.position.x = player.get_position()[0] + 300;
+			
+	if Input.is_action_pressed("ui_left"):
+		if player.velocity[0] != 0:
+			if player.position.x < 100000 && player.position.x > 340:
+				camera.position.x = player.get_position()[0];
+			if player.position.x < 340:
+				camera.position.x = 340
+
+
 var hp = 100
 var can_take_damage = true  # Prevent multiple damage hits
 
