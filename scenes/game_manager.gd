a132016e7a3ec12f91bdb74bb5505f88fbecb828
Arjun Kandadai <arjun.kandadai@gmail.com>
2025-03-14 17:35:03 -0400

    Fixed buffer when jumping. Implemented y ceiling and x limits. Able to do delayed follow with camera when jumping and falling.

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -9,31 +9,40 @@ extends Node
 
 func _process(delta):
 	#print("Player x Vel: ",player.velocity.x)
-	print("Player y Pos: ", player.position.y)
-	print("Camera y Pos: ", camera.position.y)
-
+	print("Player x Pos: ", player.position.x)
+	print("Camera x Pos: ", camera.position.x)
+	#print("Player y Pos: ", player.position.y)
+	#print("Camera y Pos: ", camera.position.y)
+	
 	if player.position.y < 2 and player.position.y > -1:
 		camera.position.y = 420
 
 	if player.is_on_floor():
 		y_pos = player.position.y;
-		camera.position.y = player.position.y + 420;
-		#camera.position.y = -400;
+		if not camera.position.y == 420:
+			if not player.position.y + 580 > 420:
+				camera.position.y = player.position.y + 540;
+				if camera.position.y < 265:
+					camera.position.y = 265
 		
+		#CAMERA CEILING 265??
 	if not player.is_on_floor():
-		if player.position.y > y_pos or player.position.y < y_pos:
+		if player.position.y > y_pos+50 or player.position.y < y_pos-50:
+			
 			camera.position.y += player.velocity.y/100;
-			#if camera.position.y >= 405: 
-				#camera.position.y = 405
+			if camera.position.y <= 265: 
+				camera.position.y = 265
 	
 	if Input.is_action_pressed("ui_right"):
-		if player.position.x < 100000 && player.position.x > 40:
+		if player.position.x + 300 < 4600 && player.position.x > 40:
 			if player.velocity[0] != 0:
 				camera.position.x = player.get_position()[0] + 300;
+			if player.position.x > 4900:
+				camera.position.x = 4600
 			
 	if Input.is_action_pressed("ui_left"):
 		if player.velocity[0] != 0:
-			if player.position.x < 100000 && player.position.x > 340:
+			if player.position.x < 4600 && player.position.x > 340:
 				camera.position.x = player.get_position()[0] + 50;
 			if player.position.x < 390:
 				camera.position.x = 340
