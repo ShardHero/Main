ddf2b92e0aa1430e8523c850c333d2c96066f105
Arjun Kandadai <arjun.kandadai@gmail.com>
2025-03-21 18:51:55 -0400

    created a global function to adjust the camera's min and max values. It is used in the _ready() function to adjust the camera's limits for the level. It works fine, but the camera node must be defined in the ready function and fed into the global function. It works fine and I don't think it's a big problem.

--- scenes/global.gd
+++ scenes/global.gd
@@ -9,5 +9,14 @@ var camera_ceiling = 265
 var camera_x_min = 340
 var camera_x_max = 4600
 
+func adjust_camera(floor, ceiling, x_min, x_max, camera_node):
+	Global.camera_floor = floor
+	Global.camera_ceiling = ceiling
+	Global.camera_x_min = x_min
+	Global.camera_x_max = x_max
+	
+	camera_node.position.x = x_min
+	camera_node.position.y = floor
+	
 func update_position(new_position):
 	spawn_position = new_position
--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -1,18 +1,9 @@
 extends Node2D
 
-func adjust_camera(floor, ceiling, x_min, x_max):
-	var camera = get_node("MovingCamera")
-	camera.position.y = floor
-	Global.camera_floor = floor
-	Global.camera_ceiling = ceiling
-	
-	camera.position.x = x_min
-	Global.camera_x_min = x_min
-	Global.camera_x_max = x_max
-	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	adjust_camera(405, 350, 390, 6500)
+	var camera = get_node("MovingCamera")
+	Global.adjust_camera(405, 350, 390, 6500, camera)
 
 	pass # Replace with function body.
 
