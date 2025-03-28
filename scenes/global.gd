a27a53e9b206d0a08ee88ec42586308d4e622cc8
coyleg152 <coyleg@rpi.edu>
2025-03-28 16:45:00 -0400

    Finished level 2

--- scenes/global.gd
+++ scenes/global.gd
@@ -7,7 +7,7 @@ var coins = 0
 var camera_floor = 420
 var camera_ceiling = 265
 var camera_x_min = -8000
-var camera_x_max = 750
+var camera_x_max = 600
 
 func update_position(new_position):
 	spawn_position = new_position
