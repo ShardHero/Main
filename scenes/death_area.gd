16bf303d1e98593a8297d28fec670bc06ec0d969
AzurDragRay <rlin1230@gmail.com>
2025-04-01 16:34:46 -0400

    Switched to better image for fade animation, scaled animation to cover entire canvas layer, and added fade logic to death area script

--- scenes/death_area.gd
+++ scenes/death_area.gd
@@ -1,9 +1,14 @@
 extends Area2D
 @onready var camera = get_node("../MovingCamera")
 @onready var player = get_node("../main_character")
+@onready var animation_player = %AnimationPlayer
+@onready var var_label = %VarLabel
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "main_character":
+		animation_player.play("fade")
+		animation_player.play("fade_in")
 		body.position = Global.spawn_position
 		camera.position.y = 420
 		camera.position.x = player.position.x + 300
