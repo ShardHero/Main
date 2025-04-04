121c8840789b2a7171fa2695cf74caec5628b71f
coyleg152 <coyleg@rpi.edu>
2025-04-04 17:09:58 -0400

    Finished Designing Level 3

--- scenes/level_3.gd
+++ scenes/level_3.gd
@@ -11,4 +11,4 @@ func _ready() -> void:
 	Global.set_var_label(var_label_node)
 	Global.update_label()
 	main_character.position = Vector2(-100,-200)
-	Global.adjust_camera(550, -100, 300, 6000, camera, player)
+	Global.adjust_camera(550, -100, 300, 8800, camera, player)
