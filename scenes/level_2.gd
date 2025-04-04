6f8d8eba92e63907b06e553b455f524a2df26690
coyleg152 <coyleg@rpi.edu>
2025-04-04 18:13:21 -0400

    Added a script to level 2
    The script sets the initial player/camera positions as well as the horizontal camera boundaries.

---
+++ scenes/level_2.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var var_label_node = $Canvas/Panel/VarLabel
+@onready var main_character: CharacterBody2D = %main_character
+@onready var player = get_node("main_character")
+@onready var camera = get_node("MovingCamera")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	Global.set_var_label(var_label_node)
+	Global.update_label()
+	main_character.position = Vector2(0, 0)
+	Global.adjust_camera(405, -134, -7500, 600, camera, player)
