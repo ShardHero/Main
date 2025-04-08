b25c53932b9a7c3cafdf59443b21b63a4c521682
AzurDragRay <rlin1230@gmail.com>
2025-04-08 16:25:52 -0400

    add fade in/out animation to level 1 in/out

--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -2,12 +2,14 @@
 extends Node
 
 @onready var var_label_node = $Canvas/Panel/VarLabel
+@onready var animation_player = %AnimationPlayer
 @onready var player = get_node("main_character")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var camera = get_node("MovingCamera")
 	#Floor, Ceiling, Xmin, Xmax
+	animation_player.play("fade_in")
 	Global.adjust_camera(405, -134, 390, 6116, camera, player)
 	Global.set_var_label(var_label_node)
 
--- scenes/level_1_up_area.gd
+++ scenes/level_1_up_area.gd
@@ -1,6 +1,9 @@
 extends Area2D
+@onready var animation_player = %AnimationPlayer
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "main_character":
+		animation_player.play("fade_out")
+		await animation_player.animation_finished # Wait for it to finish
 		get_tree().change_scene_to_file("res://scenes/toplevel_2.tscn")
