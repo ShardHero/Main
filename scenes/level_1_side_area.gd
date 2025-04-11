b08596eb45d3b11b308ed2466f2a1a1ee09676f2
AzurDragRay <rlin1230@gmail.com>
2025-04-11 18:07:53 -0400

    add fade to area_back_level1_top1, level_1_down, toplevel_2

--- scenes/level_1_side_area.gd
+++ scenes/level_1_side_area.gd
@@ -2,7 +2,6 @@ extends Area2D
 
 @onready var animation_player = %AnimationPlayer
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "main_character":
 		print("wooooooo")
--- scenes/toplevel_2.gd
+++ scenes/toplevel_2.gd
@@ -5,9 +5,15 @@ extends Node2D
 @onready var player = get_node("main_character")
 @onready var camera = get_node("MovingCamera")
 @onready var animation_player = %AnimationPlayer
+@onready var blackbox = $Canvas/blackbox
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
+	blackbox.modulate.a = 1.0
+	call_deferred("_start_scene")
+
+# Called when the node enters the scene tree for the first time.
+func _start_scene() -> void:
 	animation_player.play("fade_in")
 	Global.set_var_label(var_label_node)
 	Global.update_label()
@@ -15,6 +21,10 @@ func _ready() -> void:
 	if Global.check_dict["res://scenes/level1.tscn"].x < 5600:
 		main_character.position = Vector2(-400,0)
 		Global.adjust_camera(405,-134, -5000, 400, camera, player)
+		await get_tree().create_timer(0.5).timeout
+		animation_player.play("fade_in")
 	else:
 		main_character.position = Vector2(200,0)
 		Global.adjust_camera(405, -134, 390, 6116, camera, player)
+		await get_tree().create_timer(0.5).timeout
+		animation_player.play("fade_in")
