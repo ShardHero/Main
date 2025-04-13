1e1259126a0b59b617a2888bec0c26e730a0c53f
Tashi Sherpa <68435081+TSherpa10@users.noreply.github.com>
2025-04-12 22:12:09 -0400

    death areas + proper level teleport to level4

--- scenes/level_boss_room.gd
+++ scenes/level_boss_room.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "main_character":
-		get_tree().change_file_to_scene("boss_room_placeholder")
+		get_tree().change_scene_to_file("res://scenes/level_4.tscn")
